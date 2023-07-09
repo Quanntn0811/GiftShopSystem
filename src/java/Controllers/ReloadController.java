e7a41359388958ae5e6bcb827d5133bf5f874bd5
Datbh <buihuydat207@gmail.com>
2023-07-09 10:44:36 +0700

    Update ProductDetails

---
+++ src/java/Controllers/ReloadController.java
@@ -0,0 +1,16 @@
+
+package Controllers;
+
+import java.io.IOException;
+import jakarta.servlet.ServletException;
+import jakarta.servlet.http.HttpServlet;
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
+
+public class ReloadController extends HttpServlet {
+
+    @Override
+    protected void doGet(HttpServletRequest request, HttpServletResponse response){
+        
+    }
+}
